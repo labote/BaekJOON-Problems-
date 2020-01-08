e2a6195a18f281843c1577fc05f19265931b9854
labote <cucudok@naver.com>
2020-01-08 22:56:20 +0900

    Test 파일 이름 변경 ->  CodingTest

---
+++ CodingTest/KakaO/KakaoTest.cpp
@@ -0,0 +1,22 @@
+//
+//  KakaoTest.cpp
+//  Problems
+//
+//  Created by 홍민성 on 07/09/2019.
+//  Copyright © 2019 홍민성. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int solution(string s){
+    
+}
+
+int main(){
+    string s;
+    cin >> s;
+    
+    solution(s);
+}
